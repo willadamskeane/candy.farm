2bcae2fc7faf3b5419aa61a3dc5d7703232f75fb
Will Adams-Keane <WAK@Wills-MacBook-Pro-2.local>
2014-05-11 19:35:51 -0400

    [COMMIT_MESSAGE

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://localhost:8888/candy.farm/styles/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost:8888/candy.farm/styles/main.css">
+</head>
+
+<body>
+<div class="container">
+<div class="header">
+<div class="logo" />
+</div>
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name',function($name){
+	echo "Hello, $name";
+});
+
+$app->get('/',function(){
+	echo "<h3>Welcome to Candy Farm!</h3>";
+});
+
+
+$app->run();
+
+?>
+</div>
+</body>
+</html>
