13fc1ae374aa48021270301b2a049e7e5eede631
Will Adams-Keane <WAK@Wills-MacBook-Pro-2.local>
2014-05-13 02:13:01 -0400

    Testing jokes

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+<title>candy.farm - the worst jokes on the internet</title>
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="http://localhost:8888/candy.farm/styles/bootstrap.min.css">
-<link rel="stylesheet" href="http://localhost:8888/candy.farm/styles/main.css">
+<link rel="stylesheet" href="http://localhost:8888/candy.farm/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost:8888/candy.farm/css/main.css">
 </head>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 
 <body>
 <div class="container">
@@ -29,6 +31,10 @@ $app->get('/',function(){
 $app->run();
 
 ?>
+<div class="joke"><h4 class="joke" style='font-family:"Bodoni MT", Didot, "Didot LT STD", "Book Antiqua", Garamond, "Times New Roman", serif;'><a href="#"><span class="love glyphicon glyphicon-heart pull-left"></span></a>My dick is so big, it has its own zip code!</h4></div>
+</div>
+
+<div class="joke"><h4 class="joke" style='font-family:"Bodoni MT", Didot, "Didot LT STD", "Book Antiqua", Garamond, "Times New Roman", serif;'><a href="#"><span class="love glyphicon glyphicon-heart pull-left"></span></a>Yo mama's so fat, she can barely even walk!</h4></div>
 </div>
 </body>
 </html>
